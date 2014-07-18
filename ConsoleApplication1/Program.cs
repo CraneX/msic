4a941b1016540b0a82a0a507e28e58d7387af0cc
Crane <szcshui@gmail.com>
2014-07-17 21:26:53 -0500

    init

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.name.abc
+{
+    public class testName1
+    {
+        public int dd;
+    }
+
+}
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using test.name.abc;  // error: type or namespace "test" cannot be found.  
+public class testName2 : testName1  // build error here 
+{
+    public int id;
+}
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
